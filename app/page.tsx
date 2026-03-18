ab2eeb57cfaf7ca81caa1502391d8714b66a1a74
Govind <govind12771277@gmail.com>
2026-03-18 17:55:25 +0530

    change in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
     Hello world from Govind Gupta 
+
+    <div>
+      <Image src="https://avatars.githubusercontent.com/u/119047426?v=4" alt="Vercel Logo" width={272} height={72} priority />
+    </div>
     </div>
   );
 }
