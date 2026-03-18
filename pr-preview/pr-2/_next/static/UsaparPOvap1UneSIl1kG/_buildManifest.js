24ce8309e955e2b80a45d7b470520287c50b3e4d
Govindggupta <Govindggupta@users.noreply.github.com>
2026-03-18 12:26:19 +0000

    Deploy preview for PR 2 🛫

---
+++ pr-preview/pr-2/_next/static/UsaparPOvap1UneSIl1kG/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nextjs-pages-preview-action/pr-preview/pr-2//_next/:path+",
+        "destination": "/nextjs-pages-preview-action/pr-preview/pr-2/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
